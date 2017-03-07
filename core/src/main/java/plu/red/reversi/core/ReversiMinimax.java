e248097690fb94b3f9fdbbb8e841d305bf4f5395
Daniel Beal <git@danb.email>
2017-03-06 16:25:09 -0800

    Begin to implement the bot play mechanics

---
+++ core/src/main/java/plu/red/reversi/core/ReversiMinimax.java
@@ -0,0 +1,53 @@
+package plu.red.reversi.core;
+
+/**
+ * Created by daniel on 3/6/17.
+ * Glory to the Red Team.
+ */
+
+/**
+ * Using the minimax algorithm, calculate the optimal move for a particular player given the specified board.
+ */
+public class ReversiMinimax implements Runnable {
+
+    private Board board;
+    private Player.PlayerRole role;
+
+    private Looper.LooperCall<Board.BoardIndex> call;
+
+    // Results (TODO: More stats can go here)
+    private Board.BoardIndex bestPlay;
+
+    /**
+     * Constructs a ReversiMinimax problem to solve
+     * @param board
+     * @param role
+     */
+    public ReversiMinimax(Board board, Player.PlayerRole role) {
+        this.board = board;
+        this.role = role;
+        this.call = call;
+    }
+
+    public ReversiMinimax(Board board, Player.PlayerRole role, Looper.LooperCall<Board.BoardIndex> call) {
+        this(board, role);
+        this.call = call;
+    }
+
+    /**
+     * Calculate the best move in reversi by using the minimax algorithm.
+     *
+     * NOTE: This class implements runnable so you can export this operation to another thread quite easily.
+     * The result is returned through looper on finish if one is provided.
+     */
+    @Override
+    public void run() {
+
+        // calculate the board index of the best tile
+        // along the way, we should also generate helpful statistics
+        // which can be optionally requested from within the class itself after completion
+
+        if(call != null)
+            call.call(bestPlay);
+    }
+}
---
+++ core/src/test/java/plu/red/reversi/core/LooperTest.java
@@ -0,0 +1,82 @@
+package plu.red.reversi.core;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+/**
+ * Created by daniel on 3/6/17.
+ * Glory to the Red Team.
+ */
+public class LooperTest implements Looper.LooperCallback<Integer> {
+
+    private ArrayList<Integer> looperResults = new ArrayList<>();
+
+    @Override
+    public void onLooperCallback(Integer result) {
+        looperResults.add(result);
+    }
+
+    @Before
+    public void resetResults() {
+        looperResults.clear();
+    }
+
+    @Test
+    public void testGetLooper() {
+        Looper l1 = Looper.getLooper(Thread.currentThread());
+        Looper l2 = Looper.getLooper(Thread.currentThread());
+
+        Assert.assertTrue(l1 == l2); // we want to test that the actual references are the same
+    }
+
+    @Test
+    public void testLooperCallSingleThread() {
+
+        Looper looper = Looper.getLooper(Thread.currentThread());
+
+        Looper.LooperCall<Integer> call = looper.getCall(new Looper.LooperCallback<Integer>() {
+
+            boolean called = false;
+
+            @Override
+            public void onLooperCallback(Integer result) {
+                if(called)
+                    Assert.fail("Looper called too many times");
+
+                Assert.assertEquals(12, result.intValue());
+                called = true;
+            }
+        });
+
+        call.call(12);
+
+        Assert.assertEquals(1, looper.run());
+
+        call = looper.getCall(new Looper.LooperCallback<Integer>() {
+
+            int callCount = 1;
+
+            @Override
+            public void onLooperCallback(Integer result) {
+                Assert.assertEquals(callCount, result.intValue());
+                callCount++;
+            }
+        });
+
+        call.call(1);
+        call.call(2);
+        call.call(3);
+        call.call(4);
+        call.call(5);
+
+        Assert.assertEquals(5, looper.run());
+    }
+
+    @Test
+    public void testLooperCallMultiThread() {
+
+    }
+}
