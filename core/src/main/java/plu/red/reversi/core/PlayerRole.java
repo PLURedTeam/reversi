bd001d0989c90e5435fc2873f035ad71133166de
debroejm <debroejm@plu.edu>
2017-03-08 09:53:42 -0800

    Valid PlayerRoles
    Added functionality to determine if a PlayerRole is valid, and to get a list of valid PlayerRoles. A PlayerRole is considered valid if it represents a playable role; IE None is invalid.

--- core/src/main/java/plu/red/reversi/core/PlayerRole.java
+++ core/src/main/java/plu/red/reversi/core/PlayerRole.java
@@ -5,4 +5,28 @@ package plu.red.reversi.core;
  */
 public enum PlayerRole {
     NONE, WHITE, BLACK;
+
+    protected static final PlayerRole[] validPlayerData = new PlayerRole[]{
+            WHITE, BLACK
+    };
+
+    /**
+     * Retrieves only the PlayerRole enums that are valid players.
+     *
+     * @return Array of PlayerRole enums that are considered valid PlayerRoles
+     */
+    public static PlayerRole[] validPlayers() {
+        return validPlayerData;
+    }
+
+    /**
+     * Determines if this PlayerRole enum is a valid player.
+     *
+     * @return true if this PlayerRole is in the list of PlayerRole enums considered valid, false otherwise
+     */
+    public boolean isValid() {
+        for(PlayerRole role : validPlayerData)
+            if(role == this) return true; // Enums, so don't need equals()
+        return false;
+    }
 }
