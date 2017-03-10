8734f2a973540add0ed000244a643a26e550ef77
debroejm <debroejm@plu.edu>
2017-03-09 16:40:59 -0800

    Game Player Control
    Added some control methods for the Game class, in order to increment Player turn and get the Player whose turn it currently is.

--- core/src/main/java/plu/red/reversi/core/Game.java
+++ core/src/main/java/plu/red/reversi/core/Game.java
@@ -1,5 +1,7 @@
 package plu.red.reversi.core;
 
+import java.util.HashSet;
+
 /**
  * Glory to the Red Team.
  *
@@ -21,6 +23,8 @@ public class Game {
     //  (I realize this is probably unnecessary, but it results in more extensible code, and is easier to manipulate
     //   as a whole, instead of manipulating individual Player references)
     protected Player[] players = new Player[PlayerRole.validPlayers().length];
+    protected PlayerRole currentPlayerRole = PlayerRole.validPlayers()[0];
+    protected HashSet<PlayerRole> usedPlayers = new HashSet<PlayerRole>();
 
 
 
@@ -28,11 +32,15 @@ public class Game {
      * Constructor. Creates a new Game object.
      *
      * @param settings SettingsMap to create Game with
-     * @param board Board to create Game with
+     * @param playerCount Number of players to play this Game with
+     * @throws IllegalArgumentException if playerCount is less than 2 or more than the maximum valid PlayerRole count
      */
-    public Game(SettingsMap settings, Board board) {
+    public Game(SettingsMap settings, int playerCount) throws IllegalArgumentException {
+        if(playerCount < 2 || playerCount >= PlayerRole.validPlayers().length)
+            throw new IllegalArgumentException("Amount of Players for a game must be between 2 and " + PlayerRole.validPlayers().length);
         this.settings = settings;
-        this.board = board;
+        this.board = new Board(8);
+        for(int i = 0; i < playerCount; i++) usedPlayers.add(PlayerRole.validPlayers()[i]);
     }
 
 
@@ -65,7 +73,7 @@ public class Game {
     }
 
     /**
-     * Retrievs the player stored for the given role.
+     * Retrieves the player stored for the given role.
      *
      * @param role PlayerRole to determine Player with
      * @return Player stored for the given role, or null if no Player is stored or the PlayerRole is invalid
@@ -73,5 +81,24 @@ public class Game {
     public Player getPlayer(PlayerRole role) {
         if(role.isValid()) return players[role.validOrdinal()];
         else return null;
+    }
+
+    /**
+     * Retrieves the player whose turn it currently is.
+     *
+     * @return Current Player
+     */
+    public Player getCurrentPlayer() {
+        return players[currentPlayerRole.validOrdinal()];
+    }
+
+    /**
+     * Cycles turns and increments what player is current.
+     *
+     * @return Player whose turn is current after incrementing
+     */
+    public Player nextTurn() {
+        currentPlayerRole = currentPlayerRole.getNext(usedPlayers);
+        return players[currentPlayerRole.validOrdinal()];
     }
 }
