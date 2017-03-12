e87eb092269fd6cefe77045641125afcb383e664
Daniel Beal <git@danb.email>
2017-03-12 12:27:44 -0700

    Add requested play method

--- client/src/main/java/plu/red/reversi/client/HumanPlayer.java
+++ client/src/main/java/plu/red/reversi/client/HumanPlayer.java
@@ -1,9 +1,7 @@
 package plu.red.reversi.client;
 
 import plu.red.reversi.client.gui.GameWindow;
-import plu.red.reversi.core.Game;
-import plu.red.reversi.core.Player;
-import plu.red.reversi.core.PlayerRole;
+import plu.red.reversi.core.*;
 
 /**
  * Created by daniel on 3/5/17.
@@ -21,23 +19,24 @@ public class HumanPlayer extends Player {
 
     @Override
     public void nextTurn(boolean yours) {
-        if(yours) {
+        // IDK what we will do here
+        /*if(yours) {
             // update the GUI to make it show that the user may now input their stuff
             // TODO: Fix the GUI to use this actual input
             window.getPlayerInfoPanel().setActivePlayer(getRole() == PlayerRole.WHITE ? 0 : 1);
             window.getPlayerInfoPanel().setScore(0, getGame().getBoard().getScore(PlayerRole.WHITE));
-
-            // TODO: get previous command and update the board with nice animations
-            getGame().
-
-            // TODO: start listening for input
         }
         else {
             // set it to the player not us
             window.getPlayerInfoPanel().setActivePlayer(getRole() == PlayerRole.WHITE ? 1 : 0);
+        }*/
+    }
 
-            // TODO: Stop listening to input
+    public void issuePlay(BoardIndex index) {
+        if(getGame().getCurrentPlayer() != this) {
+            throw new IllegalStateException("Cannot play on game when not current player");
         }
 
+        getGame().acceptCommand(new CommandMove(Command.Source.PLAYER, getRole(), index));
     }
 }
