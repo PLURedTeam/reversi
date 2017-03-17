86e29069418e4cd74941a0f176a16dd4cfc0a133
Andrew <gjernear@plu.edu>
2017-03-17 09:43:18 -0700

    Added user class and some methods to the utilities

---
+++ core/src/main/java/plu/red/reversi/core/util/User.java
@@ -0,0 +1,33 @@
+package plu.red.reversi.client.util;
+
+/**
+ * Created by Andrew on 3/16/2017.
+ *
+ * Holds the user information to pass between client and server
+ * since JAX-RS will not allow me to pass two parameters using a
+ * POST request
+ */
+public class User {
+    //fields
+    private String username;
+    private String password;
+    private int sessionID;
+
+    /**
+     * Constructor for the user class
+     * Will always be constructed on the clientside and
+     * passed to the server
+     * @param u username of the player
+     * @param p password of the player stored in SHA256 format
+     */
+    public User(String u, String p) {
+        username = u;
+        password = p;
+    }//user
+
+    //Accessors and Mutators
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public int getSessionID() {return sessionID; }
+    public void setSessionID(int s) { sessionID = s; }
+}//User
