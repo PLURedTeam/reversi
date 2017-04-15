9e3333532f8e41924f88af59fb8aace91f168a02
Daniel Beal <git@danb.email>
2017-04-14 22:17:31 -0700

    Begin adding about page and preferences

---
+++ android/src/main/java/plu/red/reversi/android/SettingsFragment.java
@@ -0,0 +1,35 @@
+package plu.red.reversi.android;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Called during {@link #onCreate(Bundle)} to supply the preferences for this fragment.
+     * Subclasses are expected to call {@link #setPreferenceScreen(PreferenceScreen)} either
+     * directly or via helper methods such as {@link #addPreferencesFromResource(int)}.
+     *
+     * @param savedInstanceState If the fragment is being re-created from
+     *                           a previous saved state, this is the state.
+     * @param rootKey            If non-null, this preference fragment should be rooted at the
+     *                           {@link PreferenceScreen} with this key.
+     */
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+}
