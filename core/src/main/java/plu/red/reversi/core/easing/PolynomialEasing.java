7ce92d8aad85551e7b6900ff720b55b65ee909c6
Daniel Beal <git@danb.email>
2017-04-17 17:21:14 -0700

    Fix polynomial easing not working properly on even powers

--- core/src/main/java/plu/red/reversi/core/easing/PolynomialEasing.java
+++ core/src/main/java/plu/red/reversi/core/easing/PolynomialEasing.java
@@ -20,10 +20,13 @@ public class PolynomialEasing implements Easing {
             case EASE_IN:
                 return c * (float)Math.pow(t / d, power) + b;
             case EASE_OUT:
-                return c * ((float)Math.pow(t / d - 1, power) + 1) + b;
+                if(power % 2 == 1)
+                    return c * ((float)Math.pow(t / d - 1, power) + 1) + b;
+                else
+                    return -c * ((float)Math.pow(t / d - 1, power) - 1) + b;
             case EASE_IN_OUT:
                 if ((t/=d/2) < 1) return c / 2 * (float)Math.pow(t, power) + b;
-                return c/2*((float)Math.pow(t - 2, power) + 2) + b;
+                return -c/2*((float)Math.pow(t - 2, power) + 2) + b;
         }
 
         // should not happen.
