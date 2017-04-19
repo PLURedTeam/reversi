4d5a7d900748b26894077bb4ee484d00355e62e6
Eadword <matt@xram.co>
2017-04-19 10:13:37 -0700

    Updated logic caches to have copy constructors and a duplicate function

--- core/src/main/java/plu/red/reversi/core/game/logic/GameLogicCache.java
+++ core/src/main/java/plu/red/reversi/core/game/logic/GameLogicCache.java
@@ -8,5 +8,37 @@ import java.util.HashMap;
  * which will automatically be read if no alternative is specified.
  */
 public abstract class GameLogicCache {
-    public final HashMap<Integer, Integer> score = new HashMap<>();
+    public final HashMap<Integer, Integer> score;
+
+
+    /**
+     * Basic constructor which initializes values to their defaults.
+     */
+    public GameLogicCache() {
+        score = new HashMap<>();
+    }
+
+
+    /**
+     * Creates a copy of the current game logic which extends to the data structures, but not what they contain.
+     * For example, if an array of BoardIndexes were cached, it would copy the array so both caches can be modified
+     * independently, but it would not clone the board indexes with the assumption that they will only be replaced,
+     * not changed directly.
+     *
+     * @param other The cache to copy.
+     */
+    public GameLogicCache(GameLogicCache other) {
+        this.score = new HashMap<>(other.score);
+    }
+
+
+    /**
+     * Creates a copy of the current game logic which extends to the data structures, but not what they contain.
+     * For example, if an array of BoardIndexes were cached, it would copy the array so both caches can be modified
+     * independently, but it would not clone the board indexes with the assumption that they will only be replaced,
+     * not changed directly.
+     *
+     * @return A copy of the current GameLogicCache.
+     */
+    public abstract GameLogicCache duplicate();
 }
